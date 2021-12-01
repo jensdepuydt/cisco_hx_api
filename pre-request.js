02f6b1362616c696f635e10c5d4aca82600bdd0a
Jens Depuydt <jensdepuydt@gmail.com>
2021-12-01 11:36:41 +0100

    Add logic to replace special characters in query string
    Postman's URIEncode does not replace single quotes (which is more a problem on Intersight side).

--- pre-request.js
+++ pre-request.js
@@ -39,6 +39,45 @@ function doHttpSig() {
       return sig;
     }
 
+    // Resolve all the Postman variables that are part of the request or URI
+    let sdk = require('postman-collection');
+    let newRequest = new sdk.Request(pm.request.toJSON());
+    let resolvedRequest = newRequest.toObjectResolved(
+        null, [pm.variables.toObject()], { ignoreOwnVariables: true }
+        );
+
+    console.log("Resolved Request:");
+    console.log(resolvedRequest);
+
+    // Process Query String to properly URLencode
+    //console.log("Query String:");
+    var queryString  = "";
+    for (var i = 0; i < resolvedRequest.url.query.length; ++i) {
+            //console.log("Key:" + resolvedRequest.url.query[i].key);
+            //console.log("Value: " + resolvedRequest.url.query[i].value);
+
+            if (i>0) {
+                queryString += '&';
+            }
+            // Append each URL encoded parameter to the targetUrl unencode
+            // 24 $ Dollar Sign
+            // 28 ( Left Parenthesis
+            // 29 ) Right Parenthesis
+            // 2B + Plus Sign
+            // 2C , Comma
+            // 2F / Forward Slash
+            // 3A : Colon
+            // 3D = Equals Sign
+            // 40 @ At Sign
+            //
+            queryString += (
+                resolvedRequest.url.query[i].key + "=" +
+                encodeURIComponent(resolvedRequest.url.query[i].value).
+                replace(/['()=]/g, escape).
+                replace(/%(?:24|28|29|2B|2C|2F|3A|3D|40)/g, unescape)
+            );
+    }
+
     var curDate = new Date().toGMTString();
     var targetUrl = request.url.trim(); // there may be surrounding ws
     targetUrl = targetUrl.replace(new RegExp('^https?://[^/]+/'),'/'); // strip hostname
@@ -53,6 +92,13 @@ function doHttpSig() {
     var sha256digest = CryptoJS.SHA256(body);
     var base64sha256 = CryptoJS.enc.Base64.stringify(sha256digest);
     var computedDigest = 'SHA-256=' + base64sha256;
+
+    if (queryString.length > 0) {
+        queryStringTmp = queryString.replace("%25","%")
+        targetUrl = targetUrl.substring(0, targetUrl.indexOf('?'));
+        targetUrl += "?" + queryStringTmp;
+        console.log("Target Url: " + targetUrl );
+    }
 
     var headerHash = {
           date : curDate,
@@ -74,7 +120,6 @@ function doHttpSig() {
     postman.setEnvironmentVariable("current-date", curDate);
     postman.setEnvironmentVariable("target-url", targetUrl);
 }
-
 
 if (globals['jsrsasign-js'] === undefined ) {
     console.log("jsrasign library not already downloaded. Downloading now. ")
